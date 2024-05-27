f4bfac81acb002d92136dd4d7c9b876f3a126285
murp31 <murp31@stpiusx.nsw.edu.au>
2024-05-27 10:16:46 +1000

    created basic content page for defining problem section

---
+++ content_dp.php
@@ -0,0 +1,28 @@
+<?php
+require("header.php");
+$id=$_GET["id"];
+?>
+<html>
+<body>
+    <h1>--Defining The Problem--</h1>
+    <p>To create a Software Solution, you need to initially identify one thing: what the problem actually is.
+    This ultimately comes down to three things:</p>
+    <ul>
+        <li>The needs of the client</li>
+        <li>The objectives of the system</li>
+        <li>The limitations of the system</li>
+    </ul>
+    <h2>Needs of the Client:</h2>
+        <p>These needs are the requirements of the system: i.e. what it needs to do in order to fullfil the purpose of the system.
+        This should define the broad goals of the system, and should be consistently referenced with the purpose of the system, to determine whether each point is a preference or need.</p>
+    <h2>Objectives of the System</h2>
+        <p>These are the functional and non-functional requirements of the system which define how it should operate in order to fulfill the purpose of the system.</p>
+        <p>Non-functional requiremnts should consist of all system objectives which do not directly work to fullfil the functions of the system.</p>
+        <p>Functional requirements should consist of the technical requirments of the systems needed to fullfil it's purpose.</p>
+    <h2>Limitations of the System</h2>
+        <p>These are the practical limitations the system will face when put into practice, they can consist of technical and non-technical issues.</p>
+        <p>Technical limitations may include: network speed, storage space, clock speed.</p>
+        <p>Non-technical limitations may include: legal concerns, participant ability and/or number.</p>
+</body>
+<?php require("footer.php");?>
+</html>
--- index.php
+++ index.php
@@ -1,17 +1,21 @@
-<?php require("header.php");?>
+<?php
+require("header.php");
+$id=0;
+?>
+
 <body>
     <h1>--Contents--</h1>
     <ul>
         <li>Defining and Understanding the Problem</li>
             <ul>
-                <li><a href="dup_defining_problem.php">Defining the Problem</a></li>
-                <li><a href="dup_documenting_systems.php">Documenting Systems</a></li>
-                <li><a href="quiz.php">Topic Test</a></li>
+                <li><a href="content_dp.php?id=1">Defining the Problem</a></li>
+                <li><a href="dup_documenting_systems.php?id=2">Documenting Systems</a></li>
+                <li><a href="quiz.php?id=1">Topic Test</a></li>
             </ul>
         <li>Planning and Designing Software Solutions</li>
             <ul>
                 <li><a href="pdss_creating_psudo.php">Creating Psuedocode</a></li>
-                <li><a href="quiz.php">Topic Test</a></li>
+                <li><a href="quiz.php?id=2">Topic Test</a></li>
             </ul>
         <li>Implementation of Software Solutions</li>
         <li>Testing and Evaluating Software Solutions</li>
