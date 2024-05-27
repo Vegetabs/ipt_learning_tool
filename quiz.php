a3c53d61930c86036d4a285a569e1c3499193203
murp31 <murp31@stpiusx.nsw.edu.au>
2024-05-28 09:33:30 +1000

    added dynamic question+answer generation for quiz

--- quiz.php
+++ quiz.php
@@ -1,30 +1,70 @@
 <?php
 require("db_connect.php");
 require("header.php");
+$_SESSION["cur_q_id"] = 0;
+$conn = get_conn();
 
-FUNCTION create_quiz_arr($q_id) {
-    $conn = get_conn();
-    $sql = "SELECT quiz_id FROM quiz WHERE page_id = $q_id ORDER BY RAND() LIMIT 10";
+FUNCTION create_quiz_arr($id, $conn) {
+    $sql = "SELECT quiz_id FROM quiz WHERE topic_id = $id ORDER BY RAND() LIMIT 10";
     $result = mysqli_query($conn, $sql);
-    return mysqli_fetch_assoc($result);
+    $quiz_arr = [];
+    while ($row = mysqli_fetch_assoc($result)){
+        $quiz_arr[] = $row["quiz_id"];
+    }
+    return $quiz_arr;
+}
+
+FUNCTION get_q_content($id, $conn) {
+    $sql = "SELECT q_content FROM quiz_question WHERE q_id=$id";
+    $result = mysqli_query($conn, $sql);
+    $content = mysqli_fetch_assoc($result);
+    return $content["q_content"];
+}
+
+FUNCTION get_a_content($id, $conn) {
+    $sql = "SELECT a_content FROM quiz_answers WHERE a_id=$id";
+    $result = mysqli_query($conn, $sql);
+    $content = mysqli_fetch_assoc($result);
+    return $content["a_content"];
+}
+
+FUNCTION get_next_id() {
+    $a = $_SESSION["cur_q_id"];
+    $_SESSION["cur_q_id"] += 1;
+    return $a;
 }
 
 IF (isset($_GET["id"])) {
-    $topic_id = $_GET["id"]
-    $cur_question = 1
-    $quiz_arr = create_quiz_arr($topic_id)
-    header("Location: quiz.php?quiz_arr=$q_arr&cur_q=$cur_question&score=0")
+    $topic_id = $_GET["id"];
+    $quiz_arr = create_quiz_arr($topic_id,$conn);
+    $quiz_str = serialize($quiz_arr);
+    header("Location: quiz.php?quiz_arr=$quiz_str");
     exit;
 }
-
-IF (isset($_SESSION["question_arr"])) {
-    IF ($_SESSION["cur_question"] <= $_SESSION["num_question"]) {
-
-    } else {
-    
-    }
-} else {
-    echo("Quiz couldn't find valid questions")
-}
-
 ?>
+
+<html>
+<body>
+<form action="results.php">
+    <?php
+    $cur_question = 1;
+    $quiz_arr = unserialize($_GET["quiz_arr"]);
+    
+    while ($cur_question <= count($quiz_arr)) {
+        $question_content = "";
+        echo('<p>'.get_q_content($cur_question,$conn).'</p>
+        <input type="radio" id="'.get_next_id().'" name='.$cur_question.' value="1">
+        <label for="1">'.get_a_content(1,$conn).'</label><br>
+        <input type="radio" id="'.get_next_id().'" name='.$cur_question.' value="2">
+        <label for="2">'.get_a_content(2,$conn).'</label><br>
+        <input type="radio" id="'.get_next_id().'" name='.$cur_question.' value="3">
+        <label for="3">'.get_a_content(3,$conn).'</label><br>
+        <input type="radio" id="'.get_next_id().'" name='.$cur_question.' value="4">
+        <label for="4">'.get_a_content(4,$conn).'</label><br>');
+        $cur_question += 1;
+    }
+    ?>
+    <p></p>
+</form>
+</body>
+</html
